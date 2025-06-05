e40ee5f3f8e553e7d73fe3eb861979df080f648b
Cristian Aramayo <cristiandllanos@gmail.com>
2025-06-05 16:02:06 -0300

    ahora hice un ejercicio matemático

---
+++ Algoritmos.py
@@ -0,0 +1,5 @@
+palabra = input("Por favor, ingresá una palabra: ").capitalize()
+
+cantidad = len(palabra)
+
+print(f"Tu palabra posee {cantidad} de letras")
---
+++ Algoritmos2.py
@@ -0,0 +1,8 @@
+import math
+pi = math.pi
+
+radio = float(input("Ingresá el radio de tu circulo: "))
+
+area = math.pi * radio**2
+
+print(f"El área correspondiente es: {area}")
